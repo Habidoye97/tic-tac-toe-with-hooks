88799c4ecb4c4d0ef3c5ac53dbed45c58bf1485a
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-10-22 12:57:39 +0100

    Create game component

---
+++ src/components/Game.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Board from './Board';
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true)
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Game
